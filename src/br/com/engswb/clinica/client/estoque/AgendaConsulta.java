5b6636369cebad0d5476ab04b70589f912a30002
leandroKolesny <lekolesny@hotmail.com>
2014-06-10 21:52:26 -0300

    inicio da tela de consulta

--- src/br/com/engswb/clinica/client/estoque/AgendaConsulta.java
+++ src/br/com/engswb/clinica/client/estoque/AgendaConsulta.java
@@ -6,6 +6,17 @@ import com.google.gwt.user.client.ui.*;
 import com.google.gwt.user.datepicker.client.DateBox;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.cellview.client.CellList;
+import com.google.gwt.cell.client.AbstractCell;
+import com.google.gwt.cell.client.Cell.Context;
+import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
+import com.google.gwt.user.cellview.client.Column;
+import com.google.gwt.cell.client.SelectionCell;
+
+import java.util.ArrayList;
+
+import com.google.gwt.user.cellview.client.TextColumn;
 
 public class AgendaConsulta implements EntryPoint {
 	private AbsolutePanel absolutePanel;
@@ -13,6 +24,18 @@ public class AgendaConsulta implements EntryPoint {
 	private TextBox textBox_1;
 	private ListBox comboBox;
 	private TextBox txtIdade;
+	private TextBox textBox_2;
+	private TextBox textBox_3;
+	
+	private static class Paciente {
+		private String nome;
+		
+		public Paciente(String nome){
+			this.nome =nome;
+		}
+	}
+	
+	
 
 	@Override
 	public void onModuleLoad() {
@@ -39,6 +62,7 @@ public class AgendaConsulta implements EntryPoint {
 		absolutePanel.add(textBox, 120, 130);
 		
 		Button btnCadastrarConsulta = new SubmitButton("CadastrarConsulta");
+		btnCadastrarConsulta.setHTML("Cadastrar Consulta");
 		btnCadastrarConsulta.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
 				String nome = getTxtNome().getText();
@@ -89,6 +113,90 @@ public class AgendaConsulta implements EntryPoint {
 		AbsolutePanel absolutePanel_1 = new AbsolutePanel();
 		tabLayoutPanel.add(absolutePanel_1, "Visualizar Agendamento", false);
 		
+		AbsolutePanel absolutePanel_2 = new AbsolutePanel();
+		absolutePanel_1.add(absolutePanel_2, 10, 0);
+		absolutePanel_2.setSize("625px", "460px");
+		
+		InlineHTML nlnhtmlbuscarConsulta = new InlineHTML("<h1>Buscar Consulta</h1>");
+		absolutePanel_2.add(nlnhtmlbuscarConsulta, 6, 6);
+		
+		InlineHTML inlineHTML_2 = new InlineHTML("<hr>");
+		absolutePanel_2.add(inlineHTML_2, 6, 44);
+		
+		Label lblBuscarConsulta = new Label("Buscar consulta");
+		absolutePanel_2.add(lblBuscarConsulta, 20, 68);
+		lblBuscarConsulta.setSize("130px", "34px");
+		
+		textBox_2 = new TextBox();
+		absolutePanel_2.add(textBox_2, 6, 121);
+		
+		SubmitButton sbmtbtnBuscarConsulta = new SubmitButton("CadastrarConsulta");
+		sbmtbtnBuscarConsulta.setHTML("Buscar Consulta");
+		absolutePanel_2.add(sbmtbtnBuscarConsulta, 118, 416);
+		sbmtbtnBuscarConsulta.setSize("169px", "34px");
+		
+		InlineHTML inlineHTML_3 = new InlineHTML("Nome");
+		absolutePanel_2.add(inlineHTML_3, 6, 97);
+		
+		InlineHTML nlnhtmlId = new InlineHTML("ID da consulta");
+		absolutePanel_2.add(nlnhtmlId, 6, 161);
+		
+		textBox_3 = new TextBox();
+		absolutePanel_2.add(textBox_3, 6, 184);
+		
+		Button limparBuscaConsulta = new Button("Limpar dados");
+		limparBuscaConsulta.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				getNomeBuscaConsulta().setText("");
+				getIDBuscaConsulta().setText("");
+			}
+		});
+		absolutePanel_2.add(limparBuscaConsulta, 6, 420);
+		
+		
+		
+		
+		CellTable<Object> cellTable = new CellTable<Object>();
+		absolutePanel_2.add(cellTable, 6, 224);
+		cellTable.setSize("198px", "156px");
+		
+		
+		TextColumn<Object> textColumn = new TextColumn<Object>() {
+			@Override
+			public String getValue(Object object) {
+				return object.toString();
+			}
+		};
+		cellTable.addColumn(textColumn, "Nome:");
+		
+		TextColumn<Object> textColumn_1 = new TextColumn<Object>() {
+			@Override
+			public String getValue(Object object) {
+				return object.toString();
+			}
+		};
+		cellTable.addColumn(textColumn_1, "Data:");
+		
+		TextColumn<Object> textColumn_2 = new TextColumn<Object>() {
+			@Override
+			public String getValue(Object object) {
+				return object.toString();
+			}
+		};
+		cellTable.addColumn(textColumn_2, "Sala:");
+		
+		TextColumn<Object> textColumn_3 = new TextColumn<Object>() {
+			@Override
+			public String getValue(Object object) {
+				return object.toString();
+			}
+		};
+		cellTable.addColumn(textColumn_3, "Medico:");
+		
+		InlineHTML inlineHTML_1 = new InlineHTML("<hr>");
+		absolutePanel_2.add(inlineHTML_1, 8, 44);
+		inlineHTML_1.setSize("420px", "0px");
+		
 		AbsolutePanel absolutePanel_3 = new AbsolutePanel();
 		tabLayoutPanel.add(absolutePanel_3, "Hist\u00F3rico", false);
 		rootPanel.add(tabLayoutPanel, 10, 10);
@@ -109,5 +217,11 @@ public class AgendaConsulta implements EntryPoint {
 	}
 	public TextBox getTxtIdade() {
 		return txtIdade;
+	}
+	public TextBox getNomeBuscaConsulta() {
+		return textBox_2;
+	}
+	public TextBox getIDBuscaConsulta() {
+		return textBox_3;
 	}
 }
