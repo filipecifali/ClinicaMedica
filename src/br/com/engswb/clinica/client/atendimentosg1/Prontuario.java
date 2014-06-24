70532f5e617c2359956983b3f33cb5a7169d939d
mauriciolena <mauriciolenasi@gmail.com>
2014-06-24 20:01:31 -0300

    atualizacaoProntuario

--- src/br/com/engswb/clinica/client/atendimentosg1/Prontuario.java
+++ src/br/com/engswb/clinica/client/atendimentosg1/Prontuario.java
@@ -3,64 +3,32 @@ import org.omg.CORBA.PRIVATE_MEMBER;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dev.asm.Label;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.*;
 
 public class Prontuario implements EntryPoint {
-	private TextBox txtCPF;
-	
-	private TextBox getTxtCPF() {
-		return txtCPF;
-	}
-	private void setTxtCPF(TextBox txtCPF) {
-		this.txtCPF = txtCPF;
-	}
-	
-	
 
 	@Override
 	public void onModuleLoad() {
-
-		Label lblNome = new Label("CPF");
-		AbsolutePanel rootPanel;
-		rootPanel.add(lblNome, 47, 45);
+		// TODO Auto-generated method stub
+		RootPanel rootPanel = RootPanel.get();
 		
-		TextBox txtCPF = new TextBox();
-		rootPanel.add(txtCPF, 87, 42);
-		txtCPF.setSize("274px", "9px");
+		Label lblPronturio = new Label("Prontu\u00E1rio");
+		rootPanel.add(lblPronturio, 10, 10);
 		
-		Label lblProntuario = new Label("Prontu\u00E1rio");
-		rootPanel.add(lblProntuario, 22, 10);
-	
-		Button btnBuscar = new Button("Buscar");
-		btnBuscar.setText("Buscar");
-		rootPanel.add(btnBuscar, 49, 94);
-		btnBuscar.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				String CPF = getTxtCPF().getText();
-			}
-		}
-
-		Button btnGeraProntuario = new Button("Gerar Prontuario");
-		btnGeraProntuario.setText("Gerar Prontu\u00E1rio");
-		rootPanel.add(btnGeraProntuario, 123, 94);
-		btnGeraProntuario.setSize("159px", "30px");
-		btnGeraProntuario.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				
-			}
-			}
+		Label lblCpf = new Label("CPF:");
+		rootPanel.add(lblCpf, 13, 49);
+		
+		TextBox textBoxCPF = new TextBox();
+		rootPanel.add(textBoxCPF, 68, 49);
+		textBoxCPF.setSize("274px", "9px");
+		
+		Button btnSubmeter = new Button("Submeter");
+		rootPanel.add(btnSubmeter, 68, 114);
+		btnSubmeter.setSize("80px", "28px");
 		
 		Button btnLimpar = new Button("Limpar");
-		btnLimpar.setText("");
-		rootPanel.add(btnLimpar, 302, 94);
-		btnLimpar.setSize("57px", "30px");
-		btnLimpar.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				String CPF =  btnLimpar.setTxtCPF("");
-				// CAMPO PARA MOSTRAR RESULTADO .
-				}
-		}
-	
+		rootPanel.add(btnLimpar, 168, 114);
+		btnLimpar.setSize("80px", "28px");
+	}
 }
-}
+
